54ddbdd44a33e31185ddbd874b4a8dafc4333402
Daniel Josh <danieludumajosh@gmail.com>
2026-03-28 20:38:23 +0100

    just design

--- index.js
+++
@@ -1,8 +0,0 @@
-import { studio } from "./_studio-frame/frame.js";
-import config from "./studio.config.js";
-
-
-// Initialize Studio with config
-studio.setConfig(config);
-
-console.log("Studio Framer Initialized with Routing and Design System");
